1d3caa8faa41ad4b2591c019159f8f3cbfc485fc
CanOkar <okar.can@gmail.com>
2018-07-23 17:48:36 +0300

    Update check_events.py

--- check_events.py
+++ check_events.py
@@ -1,5 +1,9 @@
 from pytube.exceptions import RegexMatchError
+import threading
+import time
 # this function downloads videos
+
+
 def start_download(self, QMessageBox, threading, YouTube, pytube, chooseresulation, downloadbutton):
     videoAddressValue = self.videoaddress.text()
     downloadFolder = self.downloadpath.text()
@@ -35,65 +39,62 @@ def start_download(self, QMessageBox, threading, YouTube, pytube, chooseresulati
                 self.downloadbutton.setEnabled(True)
 
         # start download thread
-
         download_thread = Download_thread(chooseresulation, downloadbutton)
         download_thread.start()
 
+def start_check(self, QMessageBox, YouTube, urlretrieve, QPixmap, namelabel, videothumb,
+                chooseresulation, checkvideo, videoaddress):
+    # this function when a player hit the check button, fetchs downloadable items and pushes them into the combobox# this function when a player hit the check button, fetchs downloadable items and pushes them into the combobox
 
-# this function when a player hit the check button, fetchs downloadable items and pushes them into the combobox
-def start_check(self, QMessageBox, threading, YouTube, urlretrieve, QPixmap, namelabel, videothumb,
-                chooseresulation, checkvideo):
-    videoAddressValue = self.videoaddress.text()
+    videoAddressValue = videoaddress.text()
 
     if videoAddressValue == "":
         QMessageBox.warning(self, "Video address error", "Please paste your video address into the textbox"
-                            ,QMessageBox.Ok)
-    else:
-        # change name of "check" button with "checking" and make it deactive
-        self.checkvideo.setText("Checking")
-        self.checkvideo.setEnabled(False)
+                            , QMessageBox.Ok)
 
-        # create the class of checkVideo-thread to divide check-event from main thread
-        class CheckvideoThread(threading.Thread):
-            def __init__(self, namelabel, videothumb, chooseresulation, checkvideo):
+    else:
+
+        class CheckIt(threading.Thread):
+            def __init__(self, checkvideo, videoaddress):
                 threading.Thread.__init__(self)
-                self.namelabel = namelabel
-                self.videothumb = videothumb
-                self.chooseresulation = chooseresulation
                 self.checkvideo = checkvideo
+                self.videoaddress = videoaddress
 
             def run(self):
                 try:
+                    self.checkvideo.setText("Checking")
+                    self.checkvideo.setEnabled(False)
                     yt = YouTube(videoAddressValue)
                     yt.streams.desc()
                     allStreams = yt.streams.all()
 
                     # display video's name
                     videoTitle = yt.title
-                    self.namelabel.setText(videoTitle)
 
                     # display video's thumbnail
                     thumbnail = yt.thumbnail_url
-                    urlretrieve(thumbnail, "img.jpg")
-                    thumb = QPixmap("img.jpg")
-                    self.videothumb.setPixmap(thumb)
 
-                    # add the downloadable items into the combobox
                     for i in allStreams:
                         if str(i.audio_codec) != "None" and str(i.resolution) != "None":
                             tocombobox = str(i.resolution) + " " + str(i.mime_type) + " " + str(i.fps) + " " + str(
                                 i.itag) + \
                                          " " + str(i.audio_codec)
-                            self.chooseresulation.addItem(tocombobox)
+                            chooseresulation.addItem(tocombobox)
 
-                    # re-set check-video buttons name and make it active
+                    namelabel.setText(videoTitle)
+                    urlretrieve(thumbnail, "img.jpg")
+                    thumb = QPixmap("img.jpg")
+                    videothumb.setPixmap(thumb)
                     self.checkvideo.setText("Check Video")
                     self.checkvideo.setEnabled(True)
+
                 except RegexMatchError:
-                    print(CheckvideoThread.isAlive(self))
-                    print(CheckvideoThread.getName(self))
-                    self.checkvideo.setText("Check Video")
+                    self.videoaddress.setText("")
+                    self.checkvideo.setText("Invalid Entry")
                     self.checkvideo.setEnabled(True)
-        # start check-video thread
-        check_video_thread = CheckvideoThread(namelabel, videothumb, chooseresulation, checkvideo)
-        check_video_thread.start()
+                    time.sleep(1)
+                    self.checkvideo.setText("Check Video")
+
+
+        check_it = CheckIt(checkvideo, videoaddress)
+        check_it.start()
